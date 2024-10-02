7ba2a5e2b8162cacecc97b30be0a64531802dbcd
tobi <Tobias20.3@hotmail.de>
2024-10-02 16:18:34 +0200

    Add Parser struct to handle chunk parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use core::str;
-use std::fs::File;
-use teehistorian::{chunks::PlayerOld, Chunk, Th, ThBufReader};
-
-use twgame_core::net_msg::{self, NetVersion};
+use std::{collections::HashMap, fs::File};
+use teehistorian::{Chunk, Th, ThBufReader};
+use twgame_core::net_msg;
 
 // https://gitlab.com/ddnet-rs/twgame/-/blob/594f3f4869d34d0382ecceeaeb52cf81853ade7c/twgame-core/src/lib.rs#L93
 //     direction: input[0],
@@ -16,42 +15,63 @@ use twgame_core::net_msg::{self, NetVersion};
 //     next_weapon: input[8],
 //     prev_weapon: input[9],
 
-fn main() {
-    let f = File::open("data/random/38a7c292-76c7-42c0-bb20-cde7dd6bf373.teehistorian").unwrap();
-    // TODO: use ThCompat?
-    let mut th = Th::parse(ThBufReader::new(f)).unwrap();
+/// tracks state while parsing teehistorian file
+struct Parser {
+    /// tracks tick
+    tick: i32,
 
-    let header_bytes = th.header().unwrap();
-    // TODO: parse json
-    let header_str = str::from_utf8(header_bytes).unwrap();
+    /// tracks chunk index
+    chunk_index: u32,
 
-    let mut tick = 0;
+    /// tracks last seen cid in a player event
+    last_cid: i32,
 
-    let mut last_cid = 0; // tick is implicit if a lower cid is recorded for PLAYER_X events
+    /// tracks input vectors for each cid (for implicit ticks)
+    input_vectors: HashMap<i32, [i32; 10]>,
 
-    for index in 0..1000 {
-        let chunk = th.next_chunk().unwrap();
+    /// tracks current player sequences for each cid
+    active_sequences: HashMap<i32, PlayerSequence>,
 
+    /// stores player sequences that have been finished
+    finished_sequences: Vec<PlayerSequence>,
+}
+
+impl Parser {
+    fn new() -> Parser {
+        Parser {
+            tick: 0,
+            chunk_index: 0,
+            last_cid: 0, // TODO: Option here?
+            input_vectors: HashMap::new(),
+            active_sequences: HashMap::new(),
+            finished_sequences: Vec::new(),
+        }
+    }
+
+    fn parse_chunk(&mut self, chunk: Chunk) {
         match chunk {
             // TODO: a tick is implicit in these messages when a player with lower cid is recorded using any of PLAYER_DIFF, PLAYER_NEW, PLAYER_OLD
             Chunk::TickSkip(skip) => {
-                tick += 1 + skip.dt;
+                self.tick += 1 + skip.dt;
                 println!("> skipped {} ticks", 1 + skip.dt);
             }
             Chunk::InputNew(inp_new) => {
-                println!("[{}] cid={} -> new {:?}", index, inp_new.cid, inp_new.input);
+                println!(
+                    "[{}] cid={} -> new {:?}",
+                    self.chunk_index, inp_new.cid, inp_new.input
+                );
             }
             Chunk::InputDiff(inp_diff) => {
                 println!(
                     "[{}, {}] cid={} -> pdiff={:?}",
-                    index, tick, inp_diff.cid, inp_diff.dinput
+                    self.chunk_index, self.tick, inp_diff.cid, inp_diff.dinput
                 );
             }
             Chunk::Join(join) => {
-                println!("[{}] JOIN cid={}", index, join.cid);
+                println!("[{}] JOIN cid={}", self.chunk_index, join.cid);
             }
             Chunk::NetMessage(ref net_msg) => {
-                let res = net_msg::parse_net_msg(&net_msg.msg, &mut NetVersion::V06);
+                let res = net_msg::parse_net_msg(&net_msg.msg, &mut net_msg::NetVersion::V06);
 
                 if let Ok(res) = res {
                     match res {
@@ -69,21 +89,47 @@ fn main() {
                 }
             }
             Chunk::PlayerDiff(player_diff) => {
-                println!("[{}, {} pdiff={:?}", index, tick, player_diff);
-                if player_diff.cid <= last_cid {
-                    tick += 1;
+                println!(
+                    "[{}, {} pdiff={:?}",
+                    self.chunk_index, self.tick, player_diff
+                );
+                if player_diff.cid <= self.last_cid {
+                    self.tick += 1;
                 }
-                last_cid = player_diff.cid;
+                self.last_cid = player_diff.cid;
             }
             Chunk::PlayerNew(player_new) => {
-                println!("[{}] PLAYER NEW={:?}", index, player_new);
+                println!("[{}] PLAYER NEW={:?}", self.chunk_index, player_new);
             }
             Chunk::PlayerOld(player_old) => {
-                println!("[{}] PLAYER OLD={:?}", index, player_old);
+                println!("[{}] PLAYER OLD={:?}", self.chunk_index, player_old);
             }
             _ => {
                 println!("[?????] untracked variant: {:?}", chunk);
             }
         }
+    }
+}
+
+struct PlayerSequence {
+    player_name: Option<String>,
+    start_tick: i32,
+    inputs: Vec<[i32; 10]>,
+}
+
+fn main() {
+    let f = File::open("data/random/38a7c292-76c7-42c0-bb20-cde7dd6bf373.teehistorian").unwrap();
+    // TODO: use ThCompat?
+    let mut th = Th::parse(ThBufReader::new(f)).unwrap();
+
+    // TODO: parse json
+    let header_bytes = th.header().unwrap();
+    let header_str = str::from_utf8(header_bytes).unwrap();
+
+    let mut parser = Parser::new();
+
+    for index in 0..1000 {
+        let chunk = th.next_chunk().unwrap();
+        parser.parse_chunk(chunk);
     }
 }
