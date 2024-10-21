09717088009506254509b2ed578bfb97253595ec
tobi <Tobias20.3@hotmail.de>
2024-10-21 23:35:14 +0200

    Fixup

--- scripts/load2.py
+++ scripts/load2.py
@@ -1,14 +1,26 @@
 import teehistorian_extractor
+from teehistorian_extractor import SimpleSequence, SimplifiedTick
 import time
+
 
 # Start time
 start_time = time.time()
 
 # Call the function
-data = teehistorian_extractor.get_simplified_ticks("data/random/")
+simple_seqs = teehistorian_extractor.get_simplified_ticks("data/random/")
+
 
 # End time
 end_time = time.time()
+
+test = simple_seqs[0].start_tick
+
+print(simple_seqs[0])
+
+print(simple_seqs[0].ticks)
+tick: list[SimplifiedTick] = simple_seqs[0].ticks
+print(tick[0].fire)
+print(type(simple_seqs), type(simple_seqs[0]))
 
 # Calculate the elapsed time
 elapsed_time = end_time - start_time
